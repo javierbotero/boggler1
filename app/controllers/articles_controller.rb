0261837a62b41712c905b627ff660f61e84ceadb
beka <mr.begzodjon96@mail.ru>
2020-05-18 23:53:33 +0300

    Create New article was created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,24 @@
 class ArticlesController < ApplicationController
     #action
+    include ArticlesHelper
     def index
       @articles = Article.all
     end
 
     def create
+      @article = Article.new(article_params)
+      @article.save
+  
+      redirect_to article_path(@article)
+    end
+
+    def new
+      @article = Article.new
     end
 
     def show
       @article = Article.find(params[:id])
     end
+
 end
+
